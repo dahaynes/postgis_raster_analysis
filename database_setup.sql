6ce5a60166feeba6f626198e50214c284267fe00
David Haynes <haynesd2@gmail.com>
2017-10-17 09:42:04 -0500

    updated

---
+++ database_setup.sql
@@ -0,0 +1,8 @@
+﻿create database master;
+
+create database node1;
+create database node2;
+create database node3;
+create database node4;
+
+-- psql -U david -d node1 -c "create extension postgis;"
