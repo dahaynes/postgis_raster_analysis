fa92300884e60b563a07f55e27f5cf7dbd9e4936
David Haynes <haynesd2@gmail.com>
2018-01-15 11:22:08 -0600

    Parallel analysis works and is reporting times

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -6,17 +6,19 @@ Created on Thu Dec  7 16:27:55 2017
 """
 
 import multiprocessing as mp
-import psycopg2
+import psycopg2, timeit
 from psycopg2 import extras
+from postgresql_processing import postgis
 
-def GetConnectionParameters(aDictionary, queryText,):
+def GenerateParameters(aDictionary, queryText, placeName):
 
     """
     Generator Function
-    
     """
-    for n in aDictionary['nodes']:
-        yield aDictionary['host'], aDictionary['user'], aDictionary['port'], n, queryText
+    import itertools
+
+    for query, name, nodes in zip(queryText, placeName, itertools.cycle(aDictionary['nodes']) ):
+        yield aDictionary['host'], aDictionary['user'], aDictionary['port'], nodes, query, name
     
     
 def CreateConnection(theConnectionDict):
@@ -28,72 +30,86 @@ def CreateConnection(theConnectionDict):
 
 def NodeQuery(parameters):
     """
-        
+    This is the worker function that runs the query for every node specified    
     """
-       
-    print("Here")
-   
+    
     connectionDict = {'host': parameters[0], 'user': parameters[1], 'port': parameters[2], 'db': parameters[3] }
     queryText = parameters[4]
+    place_name = parameters[5]
     
     conn = CreateConnection(connectionDict)
     
-    print("Made connection")
-    cur = conn.cursor()
+    #print("Made connection with databases %s" % ( parameters[3]) )
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
     try:
         cur.execute(queryText)
     except:
-        print(cur.query)
-        cur.mogrify(queryText)
+        print("ERROR", cur.query)
     else:
-        results = cur.fetchall()
-        return results
+        for rec in cur:
+            #print(rec)
+            if rec['name'] == place_name:
+                value = rec
+                cur.close()
+                conn.close()
+                return value
     finally:
         cur.close()
         conn.close()
 
    
 
-def ParallelQuery(theConnection, theQuery):
+def ParallelQuery(theConnection, theQueries, thePlaces):
     """
-    This function creates breaks up a single PostgreSQL query across a variety of Nodes (Postgresql instances)
+    This function takes a pre-partitioned PostgreSQL and uses the multiprocessing module
+    to send the query across a variety of Postgresql instances.
     """
-    
-    #import itertools
-    
-    pool = mp.Pool(len(theConnection['nodes']), maxtasksperchild=1)    #Multiprocessing module
-    pool.map(NodeQuery, (i for i in GetConnectionParameters(theConnection, theQuery)) )
-#
-#    pool.map(test, [1, 2, 3] )        
-#    
-#    results = pool.join()
+    start = timeit.default_timer()  
+    try:
+        pool = mp.Pool(len(theConnection['nodes']), maxtasksperchild=1)  
+        results = pool.map(NodeQuery, (i for i in GenerateParameters(theConnection, theQueries, thePlaces)) )
+    except:
+        print(mp.get_logger())
+    else:
+        for r in results:
+            print(r)
+    finally:
+        pool.close()
+        pool.join()
+        stop = timeit.default_timer()  
+        print("Parallel Query Time %s" % (stop-start) )
+
 if __name__ == '__main__':
+    start = timeit.default_timer()        
+    connectionInfo={"host": "localhost", "db": "master", "user": "david", "port": 5432, "nodes": ["node1","node2"], "boundary_table": "states", "raster_table": "glc_250"}
+    m = postgis(connectionInfo)
+
+    query = """SELECT p.id, name, replace(replace(array_agg(r.rid)::text, '{', ''), '}', '') as raster_ids FROM states p inner join glc_250 r on ST_Intersects(r.rast, p.geom) GROUP BY p.id, name"""
+    records = m.Query(query)
+    m.DropConnection()
+
+
+    #connectionInfo={"host": "localhost", "db": "research", "user": "david", "port": 5432, "nodes": ["research","research"]}
+    #queryDict = {"boundary_table": "us_states", "srid": 4326, "raster_table": "glc2000_tilesize200", "raster_ids": [3,4,2,5]}
+    #queryDict["raster_ids"] = str(queryDict["raster_ids"]).replace("[","").replace("]","")
+    if records:
+        queryText = """ SELECT p.id, p.name, (ST_SummaryStatsAgg(ST_Clip(r.rast, p.geom), 1, True)).*
+        FROM {boundary_table} p inner join {raster_table} r on ST_Intersects(r.rast, p.geom)
+        WHERE r.rid IN ( {raster_ids} ) 
+        GROUP BY p.id, p.name """
     
+        queries = [queryText.format(**connectionInfo, **records[r]).replace("\n","") for r in records]
+        names = [records[r]['name']for r in records]
+            
+        #nQueryText = queryText.format(**queryDict).replace("\n","")
     
-    nodes = [1, 2]
-#ParallelQuery("SELECT *", nodes)
-
-    connectionInfo={"host": "localhost", "db": "research", "user": "david", "port": 5432, "nodes": ["research","research"]}
-    queryDict = {"boundary_table": "us_states", "srid": 4326, "raster_table": "glc2000_tilesize200", "raster_ids": [3,4,2,5]}
-    queryDict["raster_ids"] = str(queryDict["raster_ids"]).replace("[","").replace("]","")
-    queryText = """With boundary as 
-    (
-    SELECT gid as id, name, ST_Transform(p.geom, {srid}) as geom
-    FROM {boundary_table} p
-    )
-    SELECT p.id, p.name, (ST_SummaryStatsAgg(ST_Clip(r.rast, p.geom), 1, True)).*
-    FROM boundary p inner join {raster_table} r on ST_Intersects(r.rast, p.geom)
-    WHERE r.rid IN ( {raster_ids} )
-    GROUP BY p.id, p.name"""
-    nQueryText = queryText.format(**queryDict).replace("\n","")
-    print(nQueryText)
+        print(queries[0])
+        stopPrep = timeit.default_timer()  
+        ParallelQuery(connectionInfo, queries, names)
+    else:
+        print("No Records returned in original query")
+        print(query)
     
-    ParallelQuery(connectionInfo, nQueryText)
-#for n in connectionInfo['nodes']:
-#    print(n)
-
-#conn = psycopg2.connect(host=connectionInfo['host'], database=connectionInfo['db'], port=connectionInfo['port'], user=connectionInfo['user'])
-
-
-
-#'With boundary as ( SELECT gid as id, name, ST_Transform(p.geom, {srid}) as geom FROM {boundary_table} p ) SELECT p.id, p.name, (ST_SummaryStatsAgg(ST_Clip(r.rast, p.geom))).* FROM boundary p inner join %(raster_table)s r on ST_Intersects(r.rast, p.geom)\n    WHERE r.rid = IN (%{raster_ids}s)    GROUP BY p.id'
+    stop = timeit.default_timer()  
+    print("Data Prep Time %s" % (stopPrep-start))
+    print("TotalTime %s" % (stop-start))
