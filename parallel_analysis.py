bc5b6024f6245612edb62a45ac3e6852a5efde58
David Haynes <haynesd2@gmail.com>
2018-02-23 13:08:16 -0600

    Implemented in parallel

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -7,77 +7,119 @@ Created on Thu Dec  7 16:27:55 2017
 
 import multiprocessing as mp
 import psycopg2, timeit
-from psycopg2 import extras
 from postgresql_processing import postgis
+from copy import deepcopy
 
-def GenerateParameters(aDictionary, queryText, placeName):
+def GenerateParameters(aDictionary, queryList):
 
     """
     Generator Function
     """
-    import itertools
+    for n, query in zip(aDictionary, queryList):
+        yield aDictionary["host"], aDictionary["db"], aDictionary["user"], query
 
-    for query, name, nodes in zip(queryText, placeName, itertools.cycle(aDictionary['nodes']) ):
-        yield aDictionary['host'], aDictionary['user'], aDictionary['port'], nodes, query, name
+
+def GroupRecordsByNode(partitionedRecords): 
+    """
+    This function will takes the partiioned records and groups their information by node.
+    """   
+    nodeRecords = {}
+    for group in partitionedRecords.keys():
+        placeNames = []
+        placeRasterIds = []    
+        for rec in partitionedRecords[group].keys():
+            placeNames.append("'%s'" % (partitionedRecords[group][rec]["name"]))
+            placeRasterIds.append(partitionedRecords[group][rec]["raster_ids"])
+            
+        nodeRecords[group] = {"name": placeNames, "raster_ids": placeRasterIds}
+
+    return nodeRecords
+
+def CreateNodeQueries(partitionedQuery, nodeRecords):
+    """
+    This function will create the paritioned query that you need to send to each node.
+    """
+    nodeData = {}
+    for node in nodeRecords:
+        nameString = ",".join(nodeRecords[node]["name"])
+        rasterIDString = ",".join(nodeRecords[node]["raster_ids"])
+        uniqueID = ",".join(list(set(rasterIDString.split(","))))
+        nodeData[node] = {"place_names": nameString, "raster_ids": uniqueID}
+        
+    queries = [partitionedQuery.format(**connectionInfo, **nodeData[n]).replace("\n","") for n in nodeData]
+
+    return queries
     
     
 def CreateConnection(theConnectionDict):
     """
     This method will get a connection. Need to make sure that the DB is set correctly.
     """
+    
     connection = psycopg2.connect(host=theConnectionDict['host'], database=theConnectionDict['db'], port=theConnectionDict['port'], user=theConnectionDict['user'])
+
     return connection
 
-def NodeQuery(parameters):
+
+def ThreadedConnection(theConnectionDict):
+    """
+    This method will get a connection. Need to make sure that the DB is set correctly.
+    """
+    pooledConnect = pool.PersistentConnectionPool(1, 5, host=theConnectionDict['host'], database=theConnectionDict['db'], port=theConnectionDict['port'], user=theConnectionDict['user'])
+    
+    return pooledConnect
+
+def NodeQuery(inParameters):
     """
     This is the worker function that runs the query for every node specified    
     """
     
-    connectionDict = {'host': parameters[0], 'user': parameters[1], 'port': parameters[2], 'db': parameters[3] }
-    queryText = parameters[4]
-    place_name = parameters[5]
-    
+    connectionDict = inParameters[0]
+    sqlQuery = inParameters[1]
     conn = CreateConnection(connectionDict)
     
-    #print("Made connection with databases %s" % ( parameters[3]) )
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    try:
-        cur.execute(queryText)
-    except:
-        print("ERROR", cur.query)
+    if conn:
+        try:
+            cur = conn.cursor()
+            # cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+            cur.execute(sqlQuery)
+        except Exception as e:
+            print("ERROR", cur.query)
+        else:
+            return cur.fetchall()
+
+        finally:
+            cur.close()
+            conn.close()
     else:
-        for rec in cur:
-            #print(rec)
-            if rec['name'] == place_name:
-                value = rec
-                cur.close()
-                conn.close()
-                return value
-    finally:
-        cur.close()
-        conn.close()
+        print("No Connection Made")
 
    
 
-def ParallelQuery(theConnection, theQueries, thePlaces):
+def ParallelQuery(psqlInstances, theConnectionInfos, theQueries):
     """
     This function takes a pre-partitioned PostgreSQL and uses the multiprocessing module
     to send the query across a variety of Postgresql instances.
     """
-    start = timeit.default_timer()  
+    start = timeit.default_timer() 
+
     try:
-        pool = mp.Pool(len(theConnection['nodes']), maxtasksperchild=1)  
-        results = pool.map(NodeQuery, (i for i in GenerateParameters(theConnection, theQueries, thePlaces)) )
+        pool = mp.Pool(psqlInstances, maxtasksperchild=1)  
+        results = pool.map(NodeQuery, (i for i in zip(theConnectionInfos, theQueries)) )
     except:
+        
         print(mp.get_logger())
     else:
-        for r in results:
-            print(r)
+        pass
+        #for r in results:
+            #print(r)
     finally:
         pool.close()
         pool.join()
         stop = timeit.default_timer()  
         print("Parallel Query Time %s" % (stop-start) )
+        
+        
 
 if __name__ == '__main__':
     start = timeit.default_timer()        
@@ -88,27 +130,33 @@ if __name__ == '__main__':
     records = m.Query(query)
     m.DropConnection()
 
+    psqlInstances = len(connectionInfo['nodes'])
+    p_records = m.PartitionResults(records,psqlInstances)
+    nodeRecords = GroupRecordsByNode(p_records)
 
-    #connectionInfo={"host": "localhost", "db": "research", "user": "david", "port": 5432, "nodes": ["research","research"]}
-    #queryDict = {"boundary_table": "us_states", "srid": 4326, "raster_table": "glc2000_tilesize200", "raster_ids": [3,4,2,5]}
-    #queryDict["raster_ids"] = str(queryDict["raster_ids"]).replace("[","").replace("]","")
-    if records:
-        queryText = """ SELECT p.id, p.name, (ST_SummaryStatsAgg(ST_Clip(r.rast, p.geom), 1, True)).*
-        FROM {boundary_table} p inner join {raster_table} r on ST_Intersects(r.rast, p.geom)
-        WHERE r.rid IN ( {raster_ids} ) 
-        GROUP BY p.id, p.name """
-    
-        queries = [queryText.format(**connectionInfo, **records[r]).replace("\n","") for r in records]
-        names = [records[r]['name']for r in records]
-            
-        #nQueryText = queryText.format(**queryDict).replace("\n","")
-    
-        print(queries[0])
+
+    queryText = """ SELECT p.id, p.name, (ST_SummaryStatsAgg(ST_Clip(r.rast, p.geom), 1, True)).* 
+    FROM {boundary_table} p inner join {raster_table} r on ST_Intersects(r.rast, p.geom) 
+    WHERE r.rid IN ( {raster_ids} ) AND p.name IN ( {place_names}) 
+    GROUP BY p.id, p.name """
+
+    p_queries = CreateNodeQueries(queryText, nodeRecords)
+
+    if p_records:
+        nodeConnections = []
+        for n in connectionInfo['nodes']:
+            nodeConnection = deepcopy(connectionInfo)
+            nodeConnection['db'] = n
+            nodeConnections.append(nodeConnection)
+
+
         stopPrep = timeit.default_timer()  
-        ParallelQuery(connectionInfo, queries, names)
+        ParallelQuery(psqlInstances, nodeConnections, p_queries)
     else:
         print("No Records returned in original query")
         print(query)
+    
+
     
     stop = timeit.default_timer()  
     print("Data Prep Time %s" % (stopPrep-start))
--- postgresql_processing.py
+++ postgresql_processing.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  7 08:57:09 2017
-
+Class for paritioning information for a query.
 @author: dahaynes
 """
 
-class postgis (object):
+class postgis(object):
     
     def __init__(self, connectionDict):
         import psycopg2
@@ -102,7 +102,7 @@ class postgis (object):
             outDict = {}
             for i in range(0, len(results) - (len(results)%partitions), chunks):
                 
-                print(i, chunks)
+                # print(i, chunks)
                 #Creates the dictionary from the range
                 resultsDict = {x:results[x] for x in range(i, i + chunks) if x < len(results)}
                 #When range is 0,0 we don't want to return something
@@ -116,100 +116,30 @@ class postgis (object):
                                           
                                     
             if len(results)%partitions:
-                print("Extra")
+                # print("Extra")
                 num_records = len(results)%partitions
                 extraKeys = list(results.keys())[-num_records:]
                 
-#                #print(outDict.keys())
-#                resultsDict = {y:results[y] for y in outDict[theExtraKey]}
-#
-#                print(len(resultsDict))
-#                
+                
                 for r, key in zip(extraKeys, cycle(list(outDict.keys())) ):
                     maxKey = max(list(outDict[key].keys()))
-                    print(maxKey, r)
-                    outDict[key][maxKey+1] = results[r]
-#                
-#                del outDict[theExtraKey]
-#                finish = sum([len(outDict[d].keys()) for d in outDict])
-#                print(finish)
-#                
-#                    
-#                #if resultsDict: outDict[c+1] = resultsDict    
-#            if start == finish: 
-#                print("Awesome")
-#            else:
-#                print("Failure")
+                    # print(maxKey, r)
+                    outDict[key][maxKey+1] = results[r]#                
                                        
             return outDict
                 
         else:
             return results
+        
+
+
     
-    def PickleParameters(self, query, db):
-
-        """
-        Generator for the class
-        Input: 
-        scidbInstance = SciDB Instance IDs
-        rasterFilePath = Absolute Path to the GeoTiff
-        """
-        import itertools
-        
-        for query, n in zip(db, itertools.repeat(query)):
-            yield query, n
-    
-    def ParallelAnalysis(parameters):
-        """
-        
-        """
-        print("Here")
-        print(parameters)
-        #conn = self.GetConnection()
-
-    def ParallelQuery(self, theQuery ):
-        """
-        This function creates breaks up a single PostgreSQL query across a variety of Nodes (Postgresql instances)
-        """
-     
-        import multiprocessing as mp
-        import itertools
-        
-        pool = mp.Pool(len(self.connectionDict['nodes']), maxtasksperchild=1)    #Multiprocessing module
-                    
-        
-        pool.map(ParallelAnalysis, [1,2] )
-        pool.close()
-        pool.join()
-
-             
-#            if results:
-#                print(results, dir(results))
-#                timeDictionary  = {str(i[0]):{"version": i[0], "writeTime": i[1], "loadTime": i[2] } for i in results}
-#    
-#                return timeDictionary    
 
 
 
 
-connectionInfo={"host": "localhost", "db": "research", "user": "david", "port": 5432, "nodes": [1,2]}
 
-m = postgis(connectionInfo)
 
-query = """With boundary as 
-(
-SELECT gid as id, name, ST_Transform(p.geom, 5070) as geom
-FROM states p
-)
-
-SELECT p.id, array_agg(r.rid)
-FROM boundary p inner join nlcd_2006 r on ST_Intersects(r.rast, p.geom)
-GROUP BY p.id"""
-
-records = m.Query("SELECT * FROM raster_columns")
-dataset = m.PartitionResults(records, 2)
-m.DropConnection()
-m.ParallelQuery("SELECT * FROM raster_columns")
 
 
 
